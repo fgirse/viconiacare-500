1fb11909118847a22a78851f2f49cb04433bf5c0
fgirse <fgirs@bluewin.ch>
2026-03-30 10:57:33 +0200

    Modify Hero  /+ kalendar + graffitxy

--- src/components/landing/HeroSection6.tsx
+++ src/components/landing/HeroSection6.tsx
@@ -2,10 +2,13 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main>
-      <Spline
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <Spline
         scene="https://prod.spline.design/n0g1akardtz-Rv4y/scene.splinecode" 
       />
     </main>
   );
 }
+
+    
+
