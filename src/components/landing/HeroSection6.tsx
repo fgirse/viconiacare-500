3ed12e3a9d2bb768961516a9a23fd3d2d7380d38
fgirse <fgirs@bluewin.ch>
2026-04-02 01:58:44 +0200

    Modify Layout Teal

--- src/components/landing/HeroSection6.tsx
+++ src/components/landing/HeroSection6.tsx
@@ -3,13 +3,15 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main className="bg-[url('/Assets/Images/Philharmonie.jpg')] bg-contain bg-no-repeat bg-center w-full h-screen">
-      <Spline
-        scene="https://prod.spline.design/UBS2x8riAAkeqJwS/scene.splinecode" 
+        <Spline
+        scene="https://prod.spline.design/GMNpltbWp9KItCUl/scene.splinecode" 
         className="w-full h-full bg-linear-to-b from-transparent to-teal-900/50"
       />
     </main>
   );
 }
+
+
 
 
     
